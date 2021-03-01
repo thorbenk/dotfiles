a705b7d149733ec66a33907938d87e5678aaf8d1
Thorben Kroeger <kroeger@volumegraphics.com>
2021-03-01 22:15:25 +0100

    more tweaks

--- install.sh
+++ install.sh
@@ -82,5 +82,6 @@ ln -sfn $PWD/Xresources "$HOME/.Xresources"
 ln -sfn $PWD/i3/config "$HOME/.config/i3/config"
 ln -sfn $PWD/i3/i3status-rs.toml "$HOME/.config/i3/i3status-rs.toml"
 ln -sfn $PWD/xprofile "$HOME/.xprofile"
+ln -sfn $PWD/i3/picom.conf "$HOME/.config/picom.conf"
 
 #./install-cargo-bins.sh
