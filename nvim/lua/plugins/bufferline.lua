066a5f8ac9e41efac050fdf184f7004c6415dc07
Thorben Kroeger <kroeger@volumegraphics.com>
2023-04-14 22:28:48 +0200

    ✨ pinning in bufferline

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,17 +1,27 @@
 return {
 	{
 		"akinsho/bufferline.nvim",
-		config = function()
-			bufferline = require("bufferline")
-			bufferline.setup({
-				options = {
-					max_name_length = 50,
-					max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
-					tab_size = 21,
-					diagnostics = false, -- | "nvim_lsp" | "coc",
-					diagnostics_update_in_insert = false,
+		lazy = false,
+		keys = {
+			{ "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
+			{ "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
+		},
+		opts = {
+			options = {
+				max_name_length = 50,
+				max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
+				tab_size = 21,
+				diagnostics = false, -- | "nvim_lsp" | "coc",
+				diagnostics_update_in_insert = false,
+				offsets = {
+					{
+						filetype = "neo-tree",
+						text = "Neo-tree",
+						highlight = "Directory",
+						text_align = "left",
+					},
 				},
-			})
-		end,
+			},
+		},
 	},
 }
